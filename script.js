acbb273f4415a5cced44ab45a11eb67f8ef8e70c
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-07-11 22:10:12 +0530

    Updated with Projects & Skills.

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed('.typing', {
-    strings: ['Developer.'],
+    strings: ['Developer.', 'Coder.'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
