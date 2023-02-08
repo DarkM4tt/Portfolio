0ed164718118620bc5db97be6504ae224323f2b9
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2023-02-08 21:52:20 +0530

    Updated projects.

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
+const buttonClick = document.getElementById("btnClck");
+
+const submitHandler = () => {};
+
 $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
-      $('.navbar').addClass('sticky')
+      $(".navbar").addClass("sticky");
     } else {
-      $('.navbar').removeClass('sticky')
+      $(".navbar").removeClass("sticky");
     }
 
     // scroll-up button show/hide script
     if (this.scrollY > 500) {
-      $('.scroll-up-btn').addClass('show')
+      $(".scroll-up-btn").addClass("show");
     } else {
-      $('.scroll-up-btn').removeClass('show')
+      $(".scroll-up-btn").removeClass("show");
     }
-  })
+  });
 
   // slide-up script
-  $('.scroll-up-btn').click(function () {
-    $('html').animate({ scrollTop: 0 })
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
     // removing smooth scroll on slide-up button click
-    $('html').css('scrollBehavior', 'auto')
-  })
+    $("html").css("scrollBehavior", "auto");
+  });
 
-  $('.navbar .menu li a').click(function () {
+  $(".navbar .menu li a").click(function () {
     // applying again smooth scroll on menu items click
-    $('html').css('scrollBehavior', 'smooth')
-  })
+    $("html").css("scrollBehavior", "smooth");
+  });
 
   // toggle menu/navbar script
-  $('.menu-btn').click(function () {
-    $('.navbar .menu').toggleClass('active')
-    $('.menu-btn i').toggleClass('active')
-  })
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
   // typing text animation script
-  var typed = new Typed('.typing', {
-    strings: ['Developer.', 'Coder.'],
+  var typed = new Typed(".typing", {
+    strings: ["Developer.", "Coder."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
-  })
+  });
 
-  var typed = new Typed('.typing-2', {
+  var typed = new Typed(".typing-2", {
     strings: [
-      'Full Stack Developer.',
-      'MERN Stack Deveoper.',
-      'Web Developer.',
+      "Full Stack Developer.",
+      "MERN Stack Deveoper.",
+      "Web Developer.",
     ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
-  })
+  });
 
   // owl carousel script
-  $('.carousel').owlCarousel({
+  $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
     autoplay: true,
@@ -73,5 +77,5 @@ $(document).ready(function () {
         nav: false,
       },
     },
-  })
-})
+  });
+});
